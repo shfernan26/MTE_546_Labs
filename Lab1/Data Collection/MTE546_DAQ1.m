0efc4f257cb623914dd73a5e6283420f038de31d
shfernan26 <shfernan@uwaterloo.ca>
2023-01-30 10:43:11 -0500

    Added collected in-lab .mat and .fig files

---
+++ Lab1/Data Collection/MTE546_DAQ1.m
@@ -0,0 +1,39 @@
+%% MTE 546 Data Accquisition Code
+% University of Waterloo MTE 546
+% Written by Eugene Li Dec 2019
+close all; clear all; clc;
+%% Device Configuration
+%If this is operating correctly the board name should appear
+devices = daq.getDevices
+s = daq.createSession('ni')
+
+%Add analog inputs for each sensor being read
+addAnalogInputChannel(s,'Dev2',0,'Voltage') %Each channel corresponds to the AI on the ELVIS
+% addAnalogInputChannel(s,'Dev2',1,'Voltage')
+s.Rate = input('What is the desired Sampling Rate? e.g. 20  ')
+%% Sensor Reads
+
+% Debugging Tools
+%Do a single sensor read
+% data = s.inputSingleScan 
+
+%Read for a second
+%[data,time] = s.startForeground;
+%plot(time,data); 
+
+%Continuious Background Read
+% figure;
+% s.DurationInSeconds = 5;
+% lh = addlistener(s,'DataAvailable', @(src,event) plot(event.TimeStamps, event.Data));
+% s.NotifyWhenDataAvailableExceeds = 8000;
+% s.startBackground();
+% s.wait()
+% delete(lh);
+
+%Normal Operation
+%Read for a set period of time
+s.DurationInSeconds = 5;
+[data,time] = s.startForeground;
+figure; plot(time,data); legend('Channel 1','Channel 2');
+
+
