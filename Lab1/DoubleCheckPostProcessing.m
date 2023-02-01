4e8c613b187fb6580d0d41f8c87363a9aa34708f
arielgans <ariel.gans@gmail.com>
2023-01-31 23:12:03 -0500

    quadratic to rational

--- Lab1/DoubleCheckPostProcessing.m
+++ Lab1/DoubleCheckPostProcessing.m
@@ -1,6 +1,7 @@
 small = [s4 s10 s20 s25 s30]
 medium = [m10 m27 m45 m62 m80]
-large = [l20 l52 l85 l117 l150]
+large = [l20group8 l40group8 l80group8 l120group8 l150group8]
 mean(small)
 mean(medium)
 mean(large)
+plot([20 40 80 120 150], mean(large))
