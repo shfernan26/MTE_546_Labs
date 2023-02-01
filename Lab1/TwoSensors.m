062f9202c39551cd58b73a6d4065419ea188119e
shfernan26 <shfernan@uwaterloo.ca>
2023-02-01 10:26:45 -0500

    Modified plots for TwoSensors

--- Lab1/TwoSensors.m
+++ Lab1/TwoSensors.m
@@ -20,8 +20,8 @@ ts_30cm = data;
 syms x
 
 % Eqn that converts voltage given distance
-sr(x) = exp(-x);
-mr(x) = exp(-2*x);
+sr(x) = (0.003788)*x^2 + (-0.196472)*x + (2.799394); % Short range sensor model:
+mr(x) = (1526.721939)*( x + (63.448101) )^(-(1.501067)) + (-0.750698); % Medium range sensor model
 
 %% Calculate mean of each sensor at different distances
 short_volts = [mean(ts_10cm(:,1)) mean(ts_15cm(:,1)) mean(ts_20cm(:,1)) mean(ts_25cm(:,1)) mean(ts_30cm(:,1))];
@@ -54,7 +54,7 @@ figure(1);
 hold on
 plot(actual_dist, short_dist);
 plot(actual_dist, med_dist);
-two_sens_diff = med_dist- short_dist;
+two_sens_diff = med_dist - short_dist;
 plot(actual_dist, two_sens_diff)
 legend('Short Range','Medium Range', 'Difference between sensors')
 title('Short Range Sensor vs. Medium Range Sensor Difference')
@@ -65,33 +65,46 @@ ylabel('Measured Distance (cm)')
 % b) Comparison of sensor 1 and ruler distance
 figure(2);
 hold on
+yyaxis left
 plot(actual_dist, short_dist);
-short_err = short_dist - actual_dist;
-plot(actual_dist, short_err)
-legend('Short Range', 'Error')
 title('Short Range Sensor vs. Ruler Distance Difference')
 xlabel('True Distance (cm)')
 ylabel('Measured Distance (cm)')
+yyaxis right
+short_err = (short_dist - actual_dist).*100 ./ actual_dist;
+plot(actual_dist, short_err)
+ylabel('% Error')
 
 % c) Comparison of sensor 2 and ruler distance
 figure(3);
 hold on
+yyaxis left
 plot(actual_dist, med_dist);
-med_err = med_dist - actual_dist;
-plot(actual_dist, med_err)
-legend('Medium Range', 'Error')
 title('Medium Range Sensor vs. Ruler Distance Difference')
 xlabel('True Distance (cm)')
 ylabel('Measured Distance (cm)')
+yyaxis right
+med_err = (med_dist - actual_dist).*100 ./ actual_dist;
+plot(actual_dist, med_err)
+ylabel('% Error')
+
 
 % d) Comparison of sensor average and ruler distance
 figure(4);
 hold on
+yyaxis left
 plot(actual_dist, ave_dist);
-ave_err = ave_dist - actual_dist;
-plot(actual_dist, ave_err)
-legend('Average', 'Error')
 title('Sensor Average vs. Ruler Distance Difference')
 xlabel('True Distance (cm)')
 ylabel('Measured Distance (cm)')
+yyaxis right
+ave_err = (ave_dist - actual_dist).*100 ./ actual_dist;
+plot(actual_dist, ave_err)
+ylabel('% Error')
 
+%% Average error calculations
+ave_short_err = sprintf('Average short range error was %.2f', mean(short_err));
+disp(ave_short_err);
+
+ave_med_err = sprintf('Average medium range error was %.2f', mean(med_err));
+disp(ave_med_err);
