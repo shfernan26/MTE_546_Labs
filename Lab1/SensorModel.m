499a21d670d72fd82ab12174464144bd3457e14b
arielgans <ariel.gans@gmail.com>
2023-02-14 21:03:17 -0500

    EKF

--- Lab1/SensorModel.m
+++ Lab1/SensorModel.m
@@ -23,6 +23,10 @@ scatter(short_dist, each_short_mean);
 x0 = [50 6.4 1.3 -0.2]; 
 rational = fittype( @(a,b,c,d,x) (a*(x + b).^(-c))+d );
 [fitted_curve,gof] = fit(short_dist',each_short_mean',rational,'StartPoint',x0)
+y = fitted_curve(short_dist);
+error = abs(each_short_mean - y');
+max_error_short = max(error)
+avg_error_short = mean(error)
 p = coeffvalues(fitted_curve);
 plot(short_dist, fitted_curve(short_dist))
 hold off
@@ -30,7 +34,7 @@ legend('Collected Data','Fitted Function')
 title('Short Range Distance Means')
 xlabel('Distance (cm)')
 ylabel('Voltage (V)')
-short_eqn = sprintf('y = (%.6f) x^2 + (%.6f) x + (%.6f)',p(1),p(2),p(3));
+short_eqn = sprintf('y = (%.6f)*( x + (%.6f) )^(-(%.6f)) + (%.6f)',p(1),p(2),p(3),p(4));
 disp(short_eqn)
 
 figure(2)
@@ -40,6 +44,10 @@ scatter(med_dist, each_med_mean);
 x0 = [112703 97 2 0]; 
 rational = fittype( @(a,b,c,d,x) (a*(x + b).^(-c))+d );
 [fitted_curve,gof] = fit(med_dist',each_med_mean',rational,'StartPoint',x0)
+y = fitted_curve(med_dist);
+error = abs(each_med_mean - y');
+max_error_med = max(error)
+avg_error_med = mean(error)
 p = coeffvalues(fitted_curve);
 plot(med_dist, fitted_curve(med_dist))
 hold off
@@ -52,11 +60,20 @@ disp(med_eqn)
 
 figure(3)
 long_dist = [20 40 80 120 150];
+plot(long_dist, each_long_mean)
+title('Long Range Distance Means')
+xlabel('Distance (cm)')
+ylabel('Voltage (V)')
+figure(4)
 hold on
 scatter(long_dist, each_long_mean);
 x0 = [1 1 1 1]; 
 rational = fittype( @(a,b,c,d,x) (a*(x + b).^(-c))+d );
 [fitted_curve,gof] = fit(long_dist',each_long_mean',rational,'StartPoint',x0)
+y = fitted_curve(long_dist);
+error = abs(each_long_mean - y')
+max_error_long = max(error)
+avg_error_long = mean(error)
 p = coeffvalues(fitted_curve);
 plot(long_dist, fitted_curve(long_dist))
 hold off
